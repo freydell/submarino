93bb6c2bc7a62f6143e5681470d50071feef9c51
juanpablol15 <lopezrestrepo1@gmail.com>
2018-09-07 11:09:25 -0500

    onload

--- js/main.js
+++ js/main.js
@@ -320,7 +320,9 @@ function goPlayAgain() {
     }, 500);
 }
 
-window.addEventListener('load', function () {
+window.addEventListener('load',init);
+
+function init() {
 
     btnCredits.onclick = creditos;
     btnPlay.onclick = playGame;
@@ -354,5 +356,34 @@ window.addEventListener('load', function () {
         goToRecord('home');
     });
 
-});
+}
 
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        //init();
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
